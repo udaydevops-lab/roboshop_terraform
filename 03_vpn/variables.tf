17d4479c74734b738da1568f2ae084f7bc0024ed
udaydevops-lab <darauday0@gmail.com>
2024-09-10 16:00:37 +0530

    Initial commit with project files

---
+++ 03_vpn/variables.tf
@@ -0,0 +1,15 @@
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
